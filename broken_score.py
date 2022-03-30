b3602cd198fa232427f5fbc39e5b3471d1039537
Likyer <101633877+Likyer@users.noreply.github.com>
2022-03-30 19:32:55 +0800

    Add docstrings to broken_score

---
+++ broken_score.py
@@ -0,0 +1,26 @@
+import random
+
+dir(random)
+
+def score_status(score):
+    if score < 0:
+        print("Invalid score")
+    elif score > 100:
+        print("Invalid score")
+    elif score > 90:
+        print('Excellent')
+    elif score < 50:
+        print("Bad")
+    elif score > 50:
+        print("Passable")
+
+
+def main():
+ score = float(input("Enter score: "))
+ score_status(score)
+ generate_score()
+
+def generate_score():
+ print(random.randrange(100))
+
+main()
