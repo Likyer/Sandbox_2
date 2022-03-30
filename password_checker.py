dc338deb068dd9dffec5cc98fe5e53c4a1ca7566
Likyer <101633877+Likyer@users.noreply.github.com>
2022-03-30 19:03:07 +0800

    Add docstrings to password checker

--- password_checker.py
+++ password_checker.py
@@ -1,4 +1,12 @@
+"""
+Description: A simple Password checker
+
+Name:LIU YUHAO
+"""
+
+
 def is_vaild_password(text):
+    """Check whether a given text has the correct password format"""
     return 8 <= len(text) <= 20
 
 
@@ -7,8 +15,9 @@ def is_valid_password(new_password):
 
 
 def main():
+    """Start program"""
     new_password = "helloworld"
-    print(f"{new_password} is a valid password? {is_valid_password(new_password)}")
+    print(f"{new_password} is a valid password?{is_valid_password(new_password)}")
 
 
 if __name__ == '__main__':
